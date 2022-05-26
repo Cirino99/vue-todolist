75a67bad63a0f9fb2c5280e8ab155670dba4cacb
Simone <cirino.simone99@gmail.com>
2022-05-26 16:20:01 +0200

    creata struttura js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const myAppTodo = new Vue({
+    el : '#app',
+    data : {
+        todoList : [
+            { text : 'Dormire almeno 7 ore', done : true },
+            { text : 'Allenamento in palestra', done : false },
+            { text : 'Fare la spesa', done : false },
+            { text : 'Leggere', done : true },
+        ],
+        newItem : ''
+    },
+    methods : {
+        addItem(){
+            if(this.newItem!==''){
+                this.todoList.push({ text : this.newItem, done : false});
+                this.newItem = '';
+            }
+        },
+        removeItem(i){
+            this.todoList.splice(i,1);
+        }
+    }
+});
